452d1a51f44b9fcbc32b9fbd40da7e7c6e58d9f6
Adam Din <64247407+AdamDN29@users.noreply.github.com>
2024-06-17 17:31:12 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -5,21 +5,21 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "postgres"
+    "dialect": require('pg')
   },
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "postgres"
+    "dialect": require('pg')
   },
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "postgres",
+    "dialect": require('pg'),
     "dialectOptions": {
       "ssl": {
         "require": true,
