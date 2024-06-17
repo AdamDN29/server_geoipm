bf3cb4b0dbd9899b195183a36c573be7228a3484
Adam Din <64247407+AdamDN29@users.noreply.github.com>
2024-06-17 17:33:58 +0700

    Update dialect module

--- config/config.js
+++ config/config.js
@@ -5,21 +5,18 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": require('pg')
   },
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": require('pg')
   },
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": require('pg'),
     "dialectOptions": {
       "ssl": {
         "require": true,
